300f2fc3a4565e65e31c8fb45d87a38a69f2a160
Vinay Krishna <vinaykrishna1989@gmail.com>
2016-08-27 13:40:52 +0530

    exam related tables added

---
+++ subactivity.sql
@@ -0,0 +1,43 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 27, 2016 at 09:20 AM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `newprojectdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `subactivity`
+--
+
+CREATE TABLE IF NOT EXISTS `subactivity` (
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `ActivityId` bigint(20) NOT NULL,
+  `SubActivityName` varchar(100) NOT NULL,
+  `MaximumMark` int(11) NOT NULL,
+  `Weightage` float NOT NULL,
+  `Calculation` int(11) NOT NULL,
+  `SubActivityAvg` float DEFAULT '0',
+  `DateTimeRecordInserted` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
