1be312ee7c96d138b20e5f39d804724d6f4ec0f9
Vinay Krishna <vinaykrishna1989@gmail.com>
2016-08-27 17:25:56 +0530

    cce related tables added

---
+++ term_remark.sql
@@ -0,0 +1,40 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Aug 27, 2016 at 12:31 PM
+-- Server version: 5.6.17
+-- PHP Version: 5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `newprojectdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `term_remark`
+--
+
+CREATE TABLE IF NOT EXISTS `term_remark` (
+  `Id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `SectionId` bigint(20) NOT NULL,
+  `StudentId` bigint(20) DEFAULT NULL,
+  `Term` int(11) NOT NULL,
+  `Remark` varchar(10000) NOT NULL,
+  PRIMARY KEY (`Id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
